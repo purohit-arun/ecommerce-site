0443c7e6010d7e7b41d92e01d47c96eedf6848e2
Arun Purohit <purohitamit2001@gmail.com>
2021-05-03 16:18:32 +0530

    #4 some changes done in index.html (card related)

--- shop/views.py
+++ shop/views.py
@@ -9,17 +9,26 @@ from math import ceil
 
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4) - (n//4))
+    """ products = Product.objects.all()
+    print(products) """
+    allProds = []
+    print(allProds)
+    catProds = Product.objects.values('category','product_id')
+    print(catProds)
+    cats = {item['category'] for item in catProds}
+    print(cats)
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4) - (n//4))
+        allProds.append([prod, range(1,nSlides), nSlides])
     """ params = {
         'no_of_slides': nSlides,
         'range': range(1, nSlides),
         'product': products
     } """
-    allProds=[[products, range(1, nSlides), nSlides],
-            [products, range(1, nSlides), nSlides]]
+    """ allProds=[[products, range(1, nSlides), nSlides],
+            [products, range(1, nSlides), nSlides]] """
     params={'allProds':allProds }
     return render(request, 'shop/index.html', params)
 
