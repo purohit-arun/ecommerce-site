7c0c75e1d91577b551ea2f150fdb954259a2af3e
Arun Purohit <rajpurohitarun97@gmail.com>
2021-06-28 13:07:48 +0530

    #7(arun) chechout added

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
 
 
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,9 @@
+from typing import OrderedDict
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.db import models
-from .models import Product,Contact
+from .models import Orders, Product, Contact
 from math import ceil
 
 # Create your views here.
@@ -12,19 +13,19 @@ def index(request):
     """ products = Product.objects.all()
     print(products) """
     allProds = []
-    print('Product all prods',allProds,'\n')
-    catProds = Product.objects.values('category','product_id')
-    print('Printing catProds',catProds,'\n')
+    print('Product all prods', allProds, '\n')
+    catProds = Product.objects.values('category', 'product_id')
+    print('Printing catProds', catProds, '\n')
     cats = {item['category'] for item in catProds}
-    print("Printing cats",cats)
+    print("Printing cats", cats)
     for cat in cats:
-        prod = Product.objects.filter(category = cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
         print(n)
         nSlides = n//4 + ceil((n/4) - (n//4))
         print(nSlides)
         print(prod)
-        allProds.append([prod, range(1,nSlides), nSlides])
+        allProds.append([prod, range(1, nSlides), nSlides])
     """ params = {
         'no_of_slides': nSlides,
         'range': range(1, nSlides),
@@ -32,7 +33,7 @@ def index(request):
     } """
     """ allProds=[[products, range(1, nSlides), nSlides],
             [products, range(1, nSlides), nSlides]] """
-    params={'allProds':allProds }
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 
@@ -50,7 +51,8 @@ def contact(request):
         email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
-        contact = Contact(contact_name=name, email=email, subject=subject , message=message) 
+        contact = Contact(contact_name=name, email=email,
+                          subject=subject, message=message)
         contact.save()
     return render(request, 'shop/contact.html')
 
@@ -63,11 +65,25 @@ def search(request):
     return render(request, 'shop/search.html')
 
 
-def productView(request,id):
-    #Fetch the product using the id
-    product = Product.objects.filter(product_id = id)
+def productView(request, id):
+    # Fetch the product using the id
+    product = Product.objects.filter(product_id=id)
     return render(request, 'shop/productview.html', {'product': product[0]})
 
 
 def checkOut(request):
-    return render(request, 'shop/index.html')
+    if request.method == "POST":
+        name = request.POST.get('inputname')
+        email = request.POST.get('inputEmail4')
+        address = request.POST.get('inputAddress')
+        city = request.POST.get('inputCity')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('inputZip')
+        phone = request.POST.get('phone')
+        order = Orders(name=name, email=email, address=address,
+                       city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank' : thank, 'id' : id})    
+    return render(request, 'shop/checkout.html')
