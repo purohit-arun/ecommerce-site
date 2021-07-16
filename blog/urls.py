f3b4b0ed7315823339797bd186685a563fbaea6d
Shubham Patel <83106193+shubhamdpatel@users.noreply.github.com>
2021-07-16 16:48:18 +0530

    #2 shubham Bind BLOG data.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="BlogHome"),
+    path('', views.blog, name="blog"),
+    path('single_blog/<int:id>', views.single_blog, name="single-blog"),
     path('blogpost/<int:id>', views.blogpost, name="BlogPost"),
 ]
